bb9474a103f0ef1253ae3ddbc3bc69aee05874c4
Edwards <mapei_@hotmail.com>
2023-07-14 18:30:48 +0100

    fix build

--- UsageExample/LCDMisc_Plugin/BUILD_64.bat
+++ UsageExample/LCDMisc_Plugin/BUILD_64.bat
@@ -5,9 +5,7 @@ del *.o
 
 gcc -m64 -DLIBUSB_BLOCKS -DUSE_MMX -DHAVE_MMX -DHAVE_MMX1 -mmmx -Wno-unused-function -std=gnu11 -Wall -O2 -c RawHid.c libTeensyRawHid\libTeensyRawHid.c
 
-rem libs\libusb.a
-
-dllwrap -m64 RawHid.o libteensyRawHid.o -o RawHid_64.dll -lusb --output-def RawHid.def -k --add-stdcall-alias
+dllwrap -m64 RawHid.o libteensyRawHid.o -o RawHid_64.dll libs\libusb.a --output-def RawHid.def -k --add-stdcall-alias
 strip RawHid_64.dll
 
 
