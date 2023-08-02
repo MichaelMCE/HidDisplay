2538d13f51e7a301562d8d9cb14c20ea2aca1379
Edwards <mapei_@hotmail.com>
2023-08-02 15:11:18 +0100

    Add GDI capture method

--- UsageExample/DX9ScreenSream/BUILD_64.bat
+++ UsageExample/DX9ScreenSream/BUILD_64.bat
@@ -4,7 +4,7 @@ rem call gccpath.bat
 del *.o
 
 gcc -m64 -Wall -O3 -c screencap.c ../../libTeensyRawHid/libTeensyRawHid.c -DLIBUSB_BLOCKS -DHAVE_MMX -DHAVE_MMX1 -mmmx -Wno-unused-function -std=gnu11 -Wno-maybe-uninitialized
-gcc -m64 screencap.o libTeensyRawHid.o ../../libTeensyRawHid/libusb64.a -ldxguid -lole32 -lcomctl32 -luuid -ld3d9 -o screencap64.exe 
+gcc -m64 screencap.o libTeensyRawHid.o ../../libTeensyRawHid/libusb64.a -ldxguid -lgdi32 -lole32 -lcomctl32 -luuid -ld3d9 -o screencap64.exe 
 
 strip screencap64.exe 
 
--- UsageExample/DX9ScreenSream/screencap.c
+++ UsageExample/DX9ScreenSream/screencap.c
@@ -12,9 +12,41 @@
 
 
 
+typedef struct _d3d {
+	IDirect3D9 *d3d;
+	D3DDISPLAYMODE mode;
+	D3DPRESENT_PARAMETERS d3dpp;
+	IDirect3DDevice9 *device;
+	IDirect3DSurface9 *surface;
+	D3DLOCKED_RECT rc;
+
+	HWND hwnd;
+	uint8_t *out_buffer;
+}d3d9ctx_t;
+
+
+typedef struct _gdi {
+	HWND hwnd;
+	HDC hDCSrc;
+	HDC hDCMemory;
+	HBITMAP hBmp;
+	HBITMAP hBmpPrev;
+
+	int desktopWidth;
+	int desktopHeight; 
+
+	POINT pt;
+	RECT rc;
+	int WidthSrc;
+	int HeightSrc;
+	void *image;
+	uint8_t *out_buffer;
+}gdictx_t;
+
+
 static int DWIDTH = 0;
 static int DHEIGHT = 0;
-static teensyRawHidcxt_t ctx;
+static teensyRawHidcxt_t hid_ctx;
 
 
 
@@ -26,7 +58,7 @@ static int openDisplayWait (const int timeMs)
 	for (int i = 0; i < loops; i++){
 		Sleep(delay);
 			
-		if (libTeensyRawHid_OpenDisplay(&ctx))
+		if (libTeensyRawHid_OpenDisplay(&hid_ctx))
 			return 1;
 	}
 	return 0;
@@ -34,24 +66,24 @@ static int openDisplayWait (const int timeMs)
 
 int display_init ()
 {
-	if (!libTeensyRawHid_OpenDisplay(&ctx)){
+	if (!libTeensyRawHid_OpenDisplay(&hid_ctx)){
 		if (!openDisplayWait(500))
 			return 0;
 	}
 
 	rawhid_header_t desc;
-	libTeensyRawHid_GetConfig(&ctx, &desc);
+	libTeensyRawHid_GetConfig(&hid_ctx, &desc);
 		
-	ctx.width = desc.u.cfg.width;
-	ctx.height = desc.u.cfg.height;
-	ctx.pitch = desc.u.cfg.pitch;
-	ctx.rgbClamp = desc.u.cfg.rgbMax;
+	hid_ctx.width = desc.u.cfg.width;
+	hid_ctx.height = desc.u.cfg.height;
+	hid_ctx.pitch = desc.u.cfg.pitch;
+	hid_ctx.rgbClamp = desc.u.cfg.rgbMax;
 	
-	DWIDTH = ctx.width;
-	DHEIGHT = ctx.height;
+	DWIDTH = hid_ctx.width;
+	DHEIGHT = hid_ctx.height;
 
 	if (!DWIDTH || !DHEIGHT){
-		libTeensyRawHid_Close(&ctx);
+		libTeensyRawHid_Close(&hid_ctx);
 		return 0;
 	}
 
@@ -104,66 +136,81 @@ void frame32To16Fit (RECT *rect, uint8_t *in_pixels, uint8_t *out_pixels, const 
 	}
 }
 
-int Direct3D9Capture (const int adapter, const int bestFit, const int followCursor)
+
+void d3d9_close (d3d9ctx_t *ctx)
 {
+	if (ctx->out_buffer) free(ctx->out_buffer);
+	if (ctx->surface) IDirect3DSurface9_Release(ctx->surface);
+	if (ctx->device) IDirect3DDevice9_Release(ctx->device);
+	if (ctx->d3d) IDirect3D9_Release(ctx->d3d);
+}
 
-	IDirect3D9 *d3d = Direct3DCreate9(D3D_SDK_VERSION);
-	if (!d3d) return 0;
+int d3d9_init (d3d9ctx_t *ctx, const int adapter, POINT *pt_cursor, HWND hwnd)
+{
+	memset(ctx, 0, sizeof(*ctx));
 	
-	D3DDISPLAYMODE mode;
-	if (IDirect3D9_GetAdapterDisplayMode(d3d, adapter, &mode) != S_OK)
-		goto cleanup;
-
-	POINT pt;
-	GetCursorPos(&pt);
-	HWND hwnd = WindowFromPoint(pt);
+	ctx->hwnd = hwnd;
+	ctx->d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	if (!ctx->d3d) return 0;
 	
-	wchar_t title[256] = {0};
-	GetWindowTextW(hwnd, title, sizeof(title));
-	wprintf(L"Window: \"%ls\"\n", title);
+	if (IDirect3D9_GetAdapterDisplayMode(ctx->d3d, adapter, &ctx->mode) != S_OK){
+		d3d9_close(ctx);
+		return 0;
+	}
 	
-	D3DPRESENT_PARAMETERS d3dpp; 
-	memset(&d3dpp, 0, sizeof(d3dpp));
+	D3DPRESENT_PARAMETERS *d3dpp = &ctx->d3dpp;
+	memset(d3dpp, 0, sizeof(*d3dpp));
 	
-	d3dpp.Flags = D3DPRESENTFLAG_LOCKABLE_BACKBUFFER;
-	d3dpp.Windowed = TRUE;
-	d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	d3dpp.BackBufferCount = 1;
-	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
-	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
-	//d3dpp.EnableAutoDepthStencil = TRUE;
-	//d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+	d3dpp->Flags = D3DPRESENTFLAG_LOCKABLE_BACKBUFFER;
+	d3dpp->Windowed = TRUE;
+	d3dpp->MultiSampleType = D3DMULTISAMPLE_NONE;
+	d3dpp->SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp->BackBufferCount = 1;
+	d3dpp->PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+	d3dpp->FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
+	//d3dpp->EnableAutoDepthStencil = TRUE;
+	//d3dpp->AutoDepthStencilFormat = D3DFMT_D16;
 
-	IDirect3DDevice9 *device = NULL;
-	if (IDirect3D9_CreateDevice(d3d, adapter, D3DDEVTYPE_HAL, hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device) != S_OK)
-		goto cleanup;
+	if (IDirect3D9_CreateDevice(ctx->d3d, adapter, D3DDEVTYPE_HAL, hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, d3dpp, &ctx->device) != S_OK){
+		d3d9_close(ctx);
+		return 0;
+	}
 
-	IDirect3DSurface9 *surface = NULL;
-	if (IDirect3DDevice9_CreateOffscreenPlainSurface(device, mode.Width, mode.Height, D3DFMT_A8R8G8B8, D3DPOOL_SYSTEMMEM, &surface, NULL) != S_OK)
-		goto cleanup;
+	if (IDirect3DDevice9_CreateOffscreenPlainSurface(ctx->device, ctx->mode.Width, ctx->mode.Height, D3DFMT_A8R8G8B8, D3DPOOL_SYSTEMMEM, &ctx->surface, NULL) != S_OK){
+		d3d9_close(ctx);
+		return 0;
+	}
 
-
-	D3DLOCKED_RECT rc;
 	RECT rect;
 	int src_pitch = DWIDTH*2;
 
-	if (IDirect3DSurface9_LockRect(surface, &rc, &rect, D3DLOCK_NOSYSLOCK|D3DLOCK_NOOVERWRITE|D3DLOCK_DONOTWAIT|D3DLOCK_NO_DIRTY_UPDATE|D3DLOCK_READONLY) == S_OK){
-		src_pitch = rc.Pitch;
-		IDirect3DSurface9_UnlockRect(surface);
+	if (IDirect3DSurface9_LockRect(ctx->surface, &ctx->rc, &rect, D3DLOCK_NOSYSLOCK|D3DLOCK_NOOVERWRITE|D3DLOCK_DONOTWAIT|D3DLOCK_NO_DIRTY_UPDATE|D3DLOCK_READONLY) == S_OK){
+		src_pitch = ctx->rc.Pitch;
+		IDirect3DSurface9_UnlockRect(ctx->surface);
 	}
 
-	uint8_t *out_buffer = malloc(src_pitch * DHEIGHT);
-	if (!out_buffer) goto cleanup;
+	ctx->out_buffer = malloc(src_pitch * DHEIGHT);
+	if (!ctx->out_buffer){
+		d3d9_close(ctx);
+		return 0;
+	}
+		
+	return 1;
+}
 
+int d3d9_loop (d3d9ctx_t *ctx, const int bestFit, const int followCursor)
+{
+
+	POINT pt;
+	RECT rect;
 	RECT crect;
 	int ct = 0;
 	int32_t t0 = GetTickCount();
   	
 	while (!kbhit()){
 		if (!followCursor){
-			GetClientRect(hwnd, &crect);
-    		MapWindowPoints(HWND_DESKTOP, hwnd, (LPPOINT)&crect, 1);
+			GetClientRect(ctx->hwnd, &crect);
+    		MapWindowPoints(HWND_DESKTOP, ctx->hwnd, (LPPOINT)&crect, 1);
 
 			rect.left = abs(crect.left);
 			rect.right = rect.left + crect.right;
@@ -172,17 +219,18 @@ int Direct3D9Capture (const int adapter, const int bestFit, const int followCurs
 
 			if (rect.left < 0) rect.left = 0;
 			if (rect.top < 0) rect.top = 0;
-			if (rect.right > mode.Width-1) rect.right = mode.Width-1;
-			if (rect.bottom > mode.Height-1) rect.bottom = mode.Height-1;
+			if (rect.right > ctx->mode.Width-1) rect.right = ctx->mode.Width-1;
+			if (rect.bottom > ctx->mode.Height-1) rect.bottom = ctx->mode.Height-1;
 #if 0
 			if ((rect.bottom - rect.top)+1 > DHEIGHT) rect.bottom -= ((rect.bottom - rect.top) - DHEIGHT);
 			if ((rect.right - rect.left)+1 > DWIDTH) rect.right -= ((rect.right - rect.left) - DWIDTH);
 #endif	  
 		}else if (bestFit){
 			GetCursorPos(&pt);
-			hwnd = WindowFromPoint(pt);
-			GetClientRect(hwnd, &crect);
-			MapWindowPoints(HWND_DESKTOP, hwnd, (LPPOINT)&crect, 1);
+			ctx->hwnd = WindowFromPoint(pt);
+
+			GetClientRect(ctx->hwnd, &crect);
+			MapWindowPoints(HWND_DESKTOP, ctx->hwnd, (LPPOINT)&crect, 1);
 
 			rect.left = abs(crect.left);
 			rect.right = rect.left + crect.right;
@@ -191,56 +239,194 @@ int Direct3D9Capture (const int adapter, const int bestFit, const int followCurs
 
 			if (rect.left < 0) rect.left = 0;
 			if (rect.top < 0) rect.top = 0;
-			if (rect.right > mode.Width-1) rect.right = mode.Width-1;
-			if (rect.bottom > mode.Height-1) rect.bottom = mode.Height-1;
+			if (rect.right > ctx->mode.Width-1) rect.right = ctx->mode.Width-1;
+			if (rect.bottom > ctx->mode.Height-1) rect.bottom = ctx->mode.Height-1;
 
 		}else{		// follow cursor
 			GetCursorPos(&pt);
 			rect.left = pt.x - (DWIDTH/2);
 			if (rect.left < 0) rect.left = 0;
 			rect.right = rect.left + DWIDTH;
-			if (rect.right > mode.Width-1){
-				rect.right = mode.Width-1;
+			if (rect.right > ctx->mode.Width-1){
+				rect.right = ctx->mode.Width-1;
 				rect.left = rect.right - DWIDTH;
 			}
 		
 			rect.top = pt.y - (DHEIGHT/2);
 			if (rect.top < 0) rect.top = 0;
 			rect.bottom = rect.top + DHEIGHT;
-			if (rect.bottom > mode.Height-1){
-				rect.bottom = mode.Height-1;
+			if (rect.bottom > ctx->mode.Height-1){
+				rect.bottom = ctx->mode.Height-1;
 				rect.top = rect.bottom - DHEIGHT;
 			}
 		}
 
-		if (IDirect3DDevice9_GetFrontBufferData(device, 0, surface) == S_OK){
-			if (IDirect3DSurface9_LockRect(surface, &rc, NULL, D3DLOCK_DONOTWAIT|D3DLOCK_NO_DIRTY_UPDATE|D3DLOCK_READONLY) == S_OK){
+		if (IDirect3DDevice9_GetFrontBufferData(ctx->device, 0, ctx->surface) == S_OK){
+			if (IDirect3DSurface9_LockRect(ctx->surface, &ctx->rc, NULL, D3DLOCK_DONOTWAIT|D3DLOCK_NO_DIRTY_UPDATE|D3DLOCK_READONLY) == S_OK){
 				if (!bestFit)
-					frame32To16(&rect, rc.pBits, out_buffer, mode.Width, DWIDTH*2);
+					frame32To16(&rect, ctx->rc.pBits, ctx->out_buffer, ctx->mode.Width, DWIDTH*2);
 				else
-					frame32To16Fit(&rect, rc.pBits, out_buffer, mode.Width, DWIDTH*2);
-				IDirect3DSurface9_UnlockRect(surface);
+					frame32To16Fit(&rect, ctx->rc.pBits, ctx->out_buffer, ctx->mode.Width, DWIDTH*2);
+				IDirect3DSurface9_UnlockRect(ctx->surface);
 			}
-	  		libTeensyRawHid_WriteImage(&ctx, out_buffer);
+	  		libTeensyRawHid_WriteImage(&hid_ctx, ctx->out_buffer);
 		}else{
 			Sleep(20);
 		}
+		ct++;
+	}
+	int32_t t1 = GetTickCount();
+	printf("FPS: %.1f\n", (float)ct/(float)(t1-t0)*1000.0f);
 
-		//Sleep(120);
+	return ct;
+}
 
+int gdi_init (gdictx_t *ctx, const int desktop, POINT *pt_cursor, HWND hwnd)
+{
+	memset(ctx, 0, sizeof(*ctx));
+
+	ctx->pt = *pt_cursor;
+	ctx->hwnd = hwnd;
+
+    //Get Window device context
+	ctx->hDCSrc = GetWindowDC(ctx->hwnd);
+	GetWindowRect(ctx->hwnd, &ctx->rc);
+   	ctx->WidthSrc = ctx->rc.right - ctx->rc.left;
+	ctx->HeightSrc = ctx->rc.bottom - ctx->rc.top;
+	ctx->desktopWidth = GetDeviceCaps(ctx->hDCSrc, HORZRES);
+	ctx->desktopHeight = GetDeviceCaps(ctx->hDCSrc, VERTRES);
+	ctx->WidthSrc = max(ctx->WidthSrc, ctx->desktopWidth);
+	ctx->HeightSrc = max(ctx->HeightSrc, ctx->desktopHeight);
+
+	if (!ctx->out_buffer)
+		ctx->out_buffer = calloc(sizeof(uint32_t), ctx->desktopHeight * ctx->desktopWidth);
+	if (ctx->out_buffer == NULL) return 0;
+	ctx->image = calloc(sizeof(uint32_t), ctx->HeightSrc * ctx->WidthSrc);
+	if (ctx->image == NULL){
+		free(ctx->out_buffer);
+		return 0;
+	}
+
+    //create a memory device context
+   	ctx->hDCMemory = CreateCompatibleDC(ctx->hDCSrc);
+
+    //create a bitmap compatible with window hdc
+   	ctx->hBmp = CreateCompatibleBitmap(ctx->hDCSrc, ctx->WidthSrc, ctx->HeightSrc);
+
+	return 1;
+}
+
+int gdi_getBitmap (gdictx_t *ctx, const int bestFit, const int followCursor, const int usePrintWinAPI)
+{
+
+    //copy newly created bitmap into memory device context
+    if (!ctx->hBmpPrev)
+   		ctx->hBmpPrev = SelectObject(ctx->hDCMemory, ctx->hBmp);
+   	else
+   		SelectObject(ctx->hDCMemory, ctx->hBmp);
+    
+     // copy window hdc to memory hdc
+	if (!usePrintWinAPI)
+    	BitBlt(ctx->hDCMemory, 0, 0, ctx->WidthSrc, ctx->HeightSrc, ctx->hDCSrc, 0, 0, SRCCOPY|CAPTUREBLT);
+    else
+		PrintWindow(ctx->hwnd, ctx->hDCMemory, PW_CLIENTONLY);
+	
+    GetBitmapBits(ctx->hBmp, ctx->WidthSrc * ctx->HeightSrc * 4, ctx->image);
+
+	int x = 0;
+	int y = 0;
+	int x2 = ctx->WidthSrc-1;
+	int y2 = ctx->HeightSrc-1;
+
+
+	if (followCursor){
+		POINT *pt = &ctx->pt;
+
+		pt->x -= ctx->rc.left;
+		pt->y -= ctx->rc.top;
+
+		if (x > ctx->WidthSrc - DWIDTH) x = ctx->WidthSrc - DWIDTH;
+		if (x < 0) x = 0;
+		x2 = x+DWIDTH-1;
+
+		if (y > ctx->HeightSrc - DHEIGHT) y = ctx->HeightSrc - DHEIGHT;
+		if (y < 0) y = 0;
+		y2 = y+DHEIGHT-1;
+		
+		if (x2 > ctx->WidthSrc)
+			x2 = ctx->WidthSrc;
+			
+		if (y2 > ctx->HeightSrc)
+			y2 = ctx->HeightSrc;
+	}
+
+	RECT rect;
+	rect.left = x;
+	rect.top = y;
+	rect.right = x2;
+	rect.bottom = y2;
+
+	RECT crect;
+	GetClientRect(ctx->hwnd, &crect);
+	MapWindowPoints(HWND_DESKTOP, ctx->hwnd, (LPPOINT)&crect, 1);
+
+	rect.left += ((abs(crect.left) - ctx->rc.left));
+	rect.top += ((abs(crect.top) - ctx->rc.top));
+	rect.right = ((abs(crect.left) + crect.right) - ctx->rc.left) - 1;
+	rect.bottom = ((abs(crect.top) + crect.bottom) - ctx->rc.top) - 1;
+
+	//printf("f: %i %i %i %i, %i %i\n", rect.left, rect.right, rect.top, rect.bottom, ctx->WidthSrc, ctx->HeightSrc);
+	
+	if (!bestFit)
+		frame32To16(&rect, ctx->image, ctx->out_buffer, ctx->WidthSrc, DWIDTH*2);
+	else
+		frame32To16Fit(&rect, ctx->image, ctx->out_buffer, ctx->WidthSrc, DWIDTH*2);
+
+	ctx->hBmpPrev = NULL;
+	return 1;
+}
+
+void gdi_close (gdictx_t *ctx)
+{
+    if (ctx->hBmp)
+		DeleteObject(ctx->hBmp);
+	if (ctx->hBmpPrev)
+		DeleteObject(ctx->hBmpPrev);
+	if (ctx->hDCMemory)
+    	DeleteDC(ctx->hDCMemory);
+    if (ctx->hwnd)
+    	ReleaseDC(ctx->hwnd, ctx->hDCSrc);
+    if (ctx->image)
+    	free(ctx->image);
+    if (ctx->out_buffer)
+    	free(ctx->out_buffer);
+}
+
+int gdi_loop (gdictx_t *ctx, const int bestFit, const int followCursor, const int usePrintWinAPI)
+{
+
+	int ct = 0;
+	int32_t t0 = GetTickCount();
+
+	while (!kbhit()){
+		if (followCursor){
+			gdi_close(ctx);
+			POINT pt;
+			GetCursorPos(&pt);
+			HWND hwnd = WindowFromPoint(pt);
+			gdi_init(ctx, 0, &pt, hwnd);
+		}
+
+		if (gdi_getBitmap(ctx, bestFit, followCursor, usePrintWinAPI))
+			libTeensyRawHid_WriteImage(&hid_ctx, ctx->out_buffer);
+		else
+			Sleep(20);
 		ct++;
 	}
 	
 	int32_t t1 = GetTickCount();
 	printf("FPS: %.1f\n", (float)ct/(float)(t1-t0)*1000.0f);
-	
 
-cleanup:
-	if (out_buffer) free(out_buffer);
-	if (surface) IDirect3DSurface9_Release(surface);
-	if (device) IDirect3DDevice9_Release(device);
-	if (d3d) IDirect3D9_Release(d3d);
-	
 	return ct;
 }
 
@@ -251,9 +437,11 @@ void printHelp ()
 	printf("Usage: -option1 -option2\n");
 	printf("-fit      Scale scaptured object to display\n");
 	printf("-follow   Capture object under cursor\n");
+	printf("-gdi      Use GDI to capture\n");
+	printf("-gdi-alt  Use GDI along with PrintWindow() for capturing\n");
 	printf("-help     This\n");
+	printf("Default behaviour is to capture object under cursor at startup with D3D9\n");
 }
-
 
 int main (int margc, char **cargv)
 {
@@ -264,7 +452,10 @@ int main (int margc, char **cargv)
 
 	int bestFit = 0;
 	int followCursor = 0;
-	
+	int gdiCapture = 0;
+	int usePrintWinAPI = 0;
+	wchar_t *objectName = NULL;
+
 	for (int i = 0; i < argc; i++){
 		wchar_t *cmd = argv[i];
 		
@@ -277,8 +468,38 @@ int main (int margc, char **cargv)
 		}else if (!wcscmp(L"-help", cmd)){
 			printHelp();
 			return 0;
+			
+		}else if (!wcscmp(L"-gdi", cmd)){
+			gdiCapture = 1;
+			usePrintWinAPI = 0;
+
+		}else if (!wcscmp(L"-gdi-alt", cmd)){
+			gdiCapture = 1;
+			usePrintWinAPI = 1;
+		
+		}else if (!wcscmp(L"-name", cmd)){
+			if (++i < argc)
+				objectName = argv[i];
 		}
 	}
+
+	POINT pt;
+	GetCursorPos(&pt);
+	HWND hwnd;
+	
+	if (!objectName)
+		hwnd = WindowFromPoint(pt);
+	else
+		hwnd = FindWindowExW(NULL, NULL, objectName, NULL);
+
+	if (!hwnd){
+		printf("object not found\n");
+		return 0;
+	}
+
+	wchar_t title[512] = {0};
+	GetWindowTextW(hwnd, title, sizeof(title));
+	wprintf(L"Window: \"%ls\"\n", title);
 
 	printf("\n");
 	if (!display_init()){
@@ -286,9 +507,23 @@ int main (int margc, char **cargv)
 		return 0;
 	}
 
+	if (!gdiCapture){
+		d3d9ctx_t d3d_ctx;
+		
+		if (d3d9_init(&d3d_ctx, 0, &pt, hwnd)){
+			d3d9_loop(&d3d_ctx, bestFit, followCursor);
+			d3d9_close(&d3d_ctx);
+		}
+	}else{
+		gdictx_t gdi_ctx;
 
-	Direct3D9Capture(0, bestFit, followCursor);
+		if (gdi_init(&gdi_ctx, 0, &pt, hwnd)){
+			gdi_loop(&gdi_ctx, bestFit, followCursor, usePrintWinAPI);
+			gdi_close(&gdi_ctx);
+		}		
+	}
 
+	libTeensyRawHid_Close(&hid_ctx);
 	return 0;
 	
 }
