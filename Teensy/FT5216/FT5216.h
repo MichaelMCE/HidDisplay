c623608588fac65c95369df373f5c412af05652d
Edwards <mapei_@hotmail.com>
2023-08-06 00:18:18 +0100

    Add multi touch support

---
+++ Teensy/FT5216/FT5216.h
@@ -0,0 +1,84 @@
+#ifndef _FT5216_H_
+#define _FT5216_H_
+
+#define FT5216_DEVICE_MODE                0x00
+#define FT5216_GESTURE_ID                 0x01
+#define FT5216_TOUCH_POINTS               0x02
+
+#define FT5216_TOUCH1_EV_FLAG             0x03
+#define FT5216_TOUCH1_XH                  0x03
+#define FT5216_TOUCH1_XL                  0x04
+#define FT5216_TOUCH1_YH                  0x05
+#define FT5216_TOUCH1_YL                  0x06
+
+#define FT5216_TOUCH2_EV_FLAG             0x09
+#define FT5216_TOUCH2_XH                  0x09
+#define FT5216_TOUCH2_XL                  0x0A
+#define FT5216_TOUCH2_YH                  0x0B
+#define FT5216_TOUCH2_YL                  0x0C
+
+#define FT5216_TOUCH3_EV_FLAG             0x0F
+#define FT5216_TOUCH3_XH                  0x0F
+#define FT5216_TOUCH3_XL                  0x10
+#define FT5216_TOUCH3_YH                  0x11
+#define FT5216_TOUCH3_YL                  0x12
+
+#define FT5216_TOUCH4_EV_FLAG             0x15
+#define FT5216_TOUCH4_XH                  0x15
+#define FT5216_TOUCH4_XL                  0x16
+#define FT5216_TOUCH4_YH                  0x17
+#define FT5216_TOUCH4_YL                  0x18
+
+#define FT5216_TOUCH5_EV_FLAG             0x1B
+#define FT5216_TOUCH5_XH                  0x1B
+#define FT5216_TOUCH5_XL                  0x1C
+#define FT5216_TOUCH5_YH                  0x1D
+#define FT5216_TOUCH5_YL                  0x1E
+
+#define FT5216_REG_THGROUP                0x80   /* touch threshold related to sensitivity */
+#define FT5216_REG_THPEAK                 0X81
+#define FT5216_REG_THCAL                  0X82
+#define FT5216_REG_THWATER                0X83
+#define FT5216_REG_THTEMP                 0X84
+#define FT5216_REG_THDIFF                 0X85
+#define FT5216_REG_CTRL                   0X86
+#define FT5216_REG_TIMEENTERMONITOR       0X87
+#define FT5216_REG_PERIODACTIVE           0X88   /* report rate */
+#define FT5216_REG_PERIODMONITOR          0X89
+#define FT5216_REG_HEIGHT_B               0X8A
+#define FT5216_REG_MAX_FRAME              0X8B
+#define FT5216_REG_DIST_MOVE              0X8C
+#define FT5216_REG_DIST_POINT             0X8D
+#define FT5216_REG_FEG_FRAME              0X8E
+#define FT5216_REG_SINGLE_CLICK_OFFSET    0X8F
+#define FT5216_REG_DOUBLE_CLICK_TIME_MIN  0X90
+#define FT5216_REG_SINGLE_CLICK_TIME      0X91
+#define FT5216_REG_LEFT_RIGHT_OFFSET      0X92
+#define FT5216_REG_UP_DOWN_OFFSET         0X93
+#define FT5216_REG_DISTANCE_LEFT_RIGHT    0X94
+#define FT5216_REG_DISTANCE_UP_DOWN       0X95
+#define FT5216_REG_ZOOM_DIS_SQR           0X96
+#define FT5216_REG_RADIAN_VALUE           0X97
+#define FT5216_REG_MAX_X_HIGH             0X98
+#define FT5216_REG_MAX_X_LOW              0X99
+#define FT5216_REG_MAX_Y_HIGH             0X9A
+#define FT5216_REG_MAX_Y_LOW              0X9B
+#define FT5216_REG_K_X_HIGH               0X9C
+#define FT5216_REG_K_X_LOW                0X9D
+#define FT5216_REG_K_Y_HIGH               0X9E
+#define FT5216_REG_K_Y_LOW                0X9F
+#define FT5216_REG_AUTO_CLB_MODE          0XA0
+#define FT5216_REG_LIB_VERSION_H          0XA1
+#define FT5216_REG_LIB_VERSION_L          0XA2
+#define FT5216_REG_CIPHER                 0XA3
+#define FT5216_REG_MODE                   0XA4
+#define FT5216_REG_PMODE                  0XA5   /* Power Consume Mode        */
+#define FT5216_REG_FIRMID                 0XA6   /* Firmware version */
+#define FT5216_REG_STATE                  0XA7
+#define FT5216_REG_FT5201ID               0XA8
+#define FT5216_REG_ERR                    0XA9
+#define FT5216_REG_CLB                    0XAA
+
+
+#endif
+
--- Teensy/FT5216/FT5216.ino
+++ Teensy/FT5216/FT5216.ino
@@ -1,13 +1,20 @@
 
+/*
+	FT5216 i2c controller
+	Michael McE
+*/
+
 
 #include <Wire.h>
+#include "FT5216.h"
 
-#define FT5216_ADDR 0x38
-#define FT5216_INT	33
-
+#define FT5216_ADDR		0x38	// i2c address
+#define FT5216_INT		33		// using pin 33 for interrupt
+	
 
 typedef struct _touch {
-	int points;		// number of points (fingers) measured on panel
+	int idx;
+	int points;		// number of points (fingers) measured on panel 
 	int x;
 	int y;
 	
@@ -17,18 +24,122 @@ typedef struct _touch {
 	uint8_t yl;
 }touch_t;
 
-
-
 static volatile int touchInSignal = 0;
+static const uint8_t FT5216_RegAddrLUT[10] = {0x03, 0x09, 0x0F, 0x15, 0x1B, 0x21, 0x27, 0x2D, 0x33, 0x39};
 
-void touchISR ()
+
+
+
+void FT5216_start ()
+{
+	Wire2.beginTransmission(FT5216_ADDR);
+}
+
+int FT5216_end ()
+{
+	return Wire2.endTransmission(false);
+}
+
+int FT5216_write (const uint8_t value)
+{
+	return Wire2.write(value);
+}
+
+uint8_t FT5216_readByte ()
+{
+	return Wire2.read();
+}
+
+int FT5216_writeReg (const uint8_t reg, const uint8_t value)
+{
+	uint8_t data[] = {reg, value};
+	return Wire2.write(data, sizeof(data));
+}
+
+int FT5216_isAvailable ()
+{
+	return Wire2.available();
+}
+
+void FT5216_begin ()
+{
+	Wire2.begin();
+	Wire2.setClock(200000);
+	
+	// Delay to enter 'Monitor' status (s)
+    FT5216_writeReg(FT5216_REG_TIMEENTERMONITOR, 0x0A);
+
+    // Period of 'Active' status (ms)
+    FT5216_writeReg(FT5216_REG_PERIODACTIVE, 0x06);
+
+    // Timer to enter 'idle' when in 'Monitor' (ms)
+    FT5216_writeReg(FT5216_REG_PERIODMONITOR, 0x28);
+	
+}
+
+uint8_t FT5216_request (const uint8_t length)
+{
+	return Wire2.requestFrom((uint8_t)FT5216_ADDR, (uint8_t)length, (bool)true);
+}	
+
+void touch_ISR ()
 {
 	touchInSignal++;
 
-	Wire2.beginTransmission(FT5216_ADDR);
-	Wire2.write(0x02); // Set point to TD_STATUS 
-	if (Wire2.endTransmission(false) != 0)
+	FT5216_start();
+	FT5216_write(FT5216_TOUCH_POINTS); // Set point to TD_STATUS 
+	if (FT5216_end() != 0)
 		printf("Wire2 write error\r\n");
+}
+
+int touch_getTotal (touch_t *touch)
+{
+	uint8_t count = FT5216_request(1);
+	if (count){
+		touch->points = FT5216_readByte()&0x0F;
+		if (touch->points > (int)sizeof(FT5216_RegAddrLUT))
+			touch->points = sizeof(FT5216_RegAddrLUT);
+		return touch->points;
+	}else{
+		return 0;
+	}
+}
+
+int touch_read (touch_t *touch)
+{
+	if (touch->idx >= touch->points)
+		return 0;
+	
+	FT5216_start();
+	FT5216_write(FT5216_RegAddrLUT[touch->idx++]);
+	if (FT5216_end() != 0){
+		printf("Wire2 write error\r\n");
+		return 0;
+	}
+			
+	uint8_t count = FT5216_request(4);
+	if (!count) return 0;
+
+	int avail = FT5216_isAvailable();
+	if (avail <= 0 || avail != count){
+		printf("Wire2 read error: %i, %i\r\n", avail, count);
+		return 0;
+	}
+	
+	touch->xh = FT5216_readByte();
+	touch->xl = FT5216_readByte();
+	touch->yh = FT5216_readByte();
+	touch->yl = FT5216_readByte();
+	touch->x = (((uint16_t)touch->xh&0x0F)<<8) | touch->xl;
+   	touch->y = (((uint16_t)touch->yh&0x0F)<<8) | touch->yl;
+   	return 1;
+}
+
+void touch_begin (const int intPin, void(*cb)())
+{
+	pinMode(intPin, INPUT_PULLDOWN);
+	attachInterrupt(intPin, cb, RISING);
+	FT5216_begin();
 }
 
 void setup ()
@@ -36,39 +147,11 @@ void setup ()
 	Serial.begin(115200);
 	while (!Serial);
 
-	printf("Capacitive touchscreen started\r\n");
+	printf("FT5216 Capacitive touchscreen test\r\n");
 
-	pinMode(FT5216_INT, INPUT_PULLDOWN);
-	attachInterrupt(FT5216_INT, touchISR, RISING);
-	  
-	Wire2.begin();
-	Wire2.setClock(200000);
+	touch_begin(FT5216_INT, touch_ISR);
+
 	delay(100);
-}
-
-int getTouch (touch_t *touch)
-{
-	uint8_t count = Wire2.requestFrom((uint8_t)FT5216_ADDR, (uint8_t)5, (bool)true);
-	if (!count) return 0;
-
-	int avail = Wire2.available();
-	if (avail <= 0 || avail != count){
-		printf("Wire2 read error: %i, %i, %i\r\n", Wire2.available(), avail, count);
-		return 0;
-	}
-	
-	touch->points = Wire2.read();
-	touch->xh = Wire2.read();
-	touch->xl = Wire2.read();
-	touch->yh = Wire2.read();
-	touch->yl = Wire2.read();
-
-	if ((touch->points&0x0F) > 0){
-		touch->x = (((uint16_t)touch->xh&0x0F)<<8) | touch->xl;
-    	touch->y = (((uint16_t)touch->yh&0x0F)<<8) | touch->yl;
-    	return 1;
-	}
-	return 0;
 }
 
 void loop ()
@@ -79,9 +162,14 @@ void loop ()
 		touchInSignal = 0;
 	}
 
-
-	touch_t touch;
-	if (getTouch(&touch))
-		printf("Touch: %i, %i %i\r\n", touch.points, touch.x, touch.y);
-
+	touch_t touch = {0};
+	int tPoints = touch_getTotal(&touch);
+	if (tPoints){
+		printf("\r\n");
+		
+		for (int i = 0; i < tPoints; i++){
+			if (touch_read(&touch))
+				printf("Touch %i: %i %i\r\n", i+1, touch.x, touch.y);
+		}
+	}
 }
