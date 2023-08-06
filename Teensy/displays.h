7557ee35272a25c0902b7ce292a698188282eba7
Edwards <mapei_@hotmail.com>
2023-08-06 23:19:56 +0100

    Add inital support display buffer layers

--- Teensy/displays.h
+++ Teensy/displays.h
@@ -18,22 +18,44 @@
 
 #define CALC_PITCH_1(w)		(((w)>>3)+(((w)&0x07)!=0))	// 1bit packed, calculate number of storage bytes per row given width (of glyph)
 #define CALC_PITCH_16(w)	((w)*sizeof(uint16_t))		// 16bit (8 bits per byte)
-
 #define COLOUR_24TO16(c)	((uint16_t)(((((c)>>16)&0xF8)<<8) | ((((c)>>8)&0xFC)<<3) | (((c)&0xF8)>>3)))
 
+// call me firstmost
+void tft_init ();
 
-
+// send buffer to display
 void tft_update ();
 void tft_update_area (uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
-void tft_init ();
+
+// clear display + buffer
 void tft_clear (const uint16_t colour);
+
+// clear buffer only
 void tft_clearBuffer (const uint16_t colour);
+
+// set display rotation. this does not rotate the buffer.
 void tft_rotate (const uint8_t rotation);
+
+// get a pointer to active layer
 uint8_t *tft_getBuffer ();
 
+// send an array/buffer directly to display, to the destination area
 void tft_update_array (uint16_t *pixels, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+
+// adjust backlight PWN level. range is 0 to 127
 void tft_backlight (const uint8_t level);
 
+// set active layer
+int tft_setWriteLayer (const int layerIdx);
+
+// return active layer
+int tft_getWriteLayer ();
+
+// switch between layers 1 and 0
+int tft_toggleLayer ();
+
+// activate next layer in chain or back to 0 if end
+int tft_nextLayer ();
 
 
 void oled_init ();
--- Teensy/tft_display.cpp
+++ Teensy/tft_display.cpp
@@ -15,10 +15,58 @@
 #define TFT_HEIGHT_SPLIT (TFT_HEIGHT)
 #endif
 
+#if (USE_EXTMEM_BUFFER && USE_BUFFER_LAYERS)
+#define layersTotal (BUFFER_LAYERS_TOTAL)
+static int layersActive = 0;
+#else
+#define layersTotal (1)
+static int layersActive = 0;
+#endif
 
-static uint8_t STORAGETYPE tft_buffer[CALC_PITCH_16(TFT_WIDTH)*TFT_HEIGHT_SPLIT];
+static uint8_t STORAGETYPE tft_buffer[layersTotal][CALC_PITCH_16(TFT_WIDTH)*TFT_HEIGHT_SPLIT];
 
 
+
+/*
+
+
+lsyers_setActive
+lsyers_getActive
+lsyers_swap
+lsyers_next
+
+*/
+
+int tft_setWriteLayer (const int layerIdx)
+{
+	int old = layersActive;
+	if (layerIdx < layersTotal)
+		layersActive = layerIdx;
+
+	return old;
+}
+
+int tft_getWriteLayer ()
+{
+	return layersActive;
+}
+
+int tft_nextLayer ()
+{
+	int nextLayer = layersActive + 1;
+	if (nextLayer >= layersTotal)
+		nextLayer = 0;
+
+	layersActive = nextLayer;
+	return tft_getWriteLayer();
+}
+
+int tft_toggleLayer ()
+{
+	int nextLayer = (!tft_getWriteLayer())&0x01;
+	tft_setWriteLayer(nextLayer);
+	return tft_getWriteLayer();
+}
 
 void tft_init ()
 {
@@ -31,6 +79,7 @@ void tft_init ()
 	FLEXSPI2_AHBRXBUF2CR0 = 0;
 	FLEXSPI2_AHBRXBUF3CR0 = FLEXSPI_AHBRXBUFCR0_PREFETCHEN;
 #endif
+
 	// increase PSRAM clocks to max stable, ~130MHZ
 	CCM_CBCMR &= ~(CCM_CBCMR_FLEXSPI2_PODF_MASK | CCM_CBCMR_FLEXSPI2_CLK_SEL_MASK);		// clear settings
 	CCM_CBCMR |=  (CCM_CBCMR_FLEXSPI2_PODF(3)   | CCM_CBCMR_FLEXSPI2_CLK_SEL(3));		// 133 MHz
@@ -41,12 +90,12 @@ void tft_init ()
 
 void tft_update ()
 {
-	lcd.pushPixels16bit((uint16_t*)tft_buffer, 0, 0, TFT_WIDTH-1, TFT_HEIGHT_SPLIT-1);
+	lcd.pushPixels16bit((uint16_t*)tft_buffer[layersActive], 0, 0, TFT_WIDTH-1, TFT_HEIGHT_SPLIT-1);
 }
 
 void tft_update_area (uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 {
-	lcd.pushPixels16bit((uint16_t*)tft_buffer, x1, y1, x2, y2);
+	lcd.pushPixels16bit((uint16_t*)tft_buffer[layersActive], x1, y1, x2, y2);
 }
 
 static inline void tft_clearFrame (uint16_t *pixels, const uint16_t colour)
@@ -58,13 +107,13 @@ static inline void tft_clearFrame (uint16_t *pixels, const uint16_t colour)
 
 void tft_clearBuffer (const uint16_t colour)
 {
-	uint16_t *pixels = (uint16_t*)tft_buffer;
+	uint16_t *pixels = (uint16_t*)tft_buffer[layersActive];
 	tft_clearFrame(pixels, colour);
 }
 
 void tft_clear (const uint16_t colour)
 {
-	uint16_t *pixels = (uint16_t*)tft_buffer;
+	uint16_t *pixels = (uint16_t*)tft_buffer[layersActive];
 	tft_clearFrame(pixels, colour);
 
 	for (int y = 0; y < TFT_HEIGHT; y++)
@@ -73,7 +122,7 @@ void tft_clear (const uint16_t colour)
 
 uint8_t *tft_getBuffer ()
 {
-	return (uint8_t*)tft_buffer;
+	return (uint8_t*)tft_buffer[layersActive];
 }
 
 void tft_rotate (const uint8_t rotation)
