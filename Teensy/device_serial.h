e66db0b6a47197ae89d91bc46e7d6c3154216dda
Edwards <mapei_@hotmail.com>
2023-08-08 03:10:10 +0100

    Add device serial number capability

---
+++ Teensy/device_serial.h
@@ -0,0 +1,15 @@
+
+#ifndef _DEVICE_SERIAL_H
+#define _DEVICE_SERIAL_H
+
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+
+#define DEVICE_SERIAL_NUM	10001
+#define DEVICE_SERIAL_STR	STR(DEVICE_SERIAL_NUM)
+
+
+#endif
+
