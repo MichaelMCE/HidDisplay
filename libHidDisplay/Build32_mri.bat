d914cd8d5f064f16d8d92f1fe8de200188ca2469
MichaelMcE <mapei_@hotmail.com>
2025-06-29 00:42:16 +0100

    Add binary build scripts

---
+++ libHidDisplay/Build32_mri.bat
@@ -0,0 +1,15 @@
+@echo off
+
+
+gcc -m32 -c -O2 libHidDisplay.c
+gcc -m32 -c -O2 libHidDisplayAsync.c
+
+gcc -m32 -shared -Wl,--output-def=libhiddisplay.def -Wl,--dll -Wl,--out-implib=libhiddisplay.dll.a libHidDisplayAsync.o libHidDisplay.o libusb32.a -o hiddisplay.dll
+ar rc libhiddisplay_tmp.a libHidDisplayAsync.o libHidDisplay.o
+
+ar -M <pack32.mri
+ranlib libhiddisplay.a
+
+del libhiddisplay_tmp.a
+del libHidDisplayAsync.o 
+del libHidDisplay.o
---
+++ libHidDisplay/Build64_mri.bat
@@ -0,0 +1,15 @@
+@echo off
+
+
+gcc -m64 -c -O2 libHidDisplay.c
+gcc -m64 -c -O2 libHidDisplayAsync.c
+
+gcc -m64 -shared -Wl,--output-def=libhiddisplay.def -Wl,--dll -Wl,--out-implib=libhiddisplay.dll.a libHidDisplayAsync.o libHidDisplay.o libusb64.a -o hiddisplay.dll
+ar rc libhiddisplay_tmp.a libHidDisplayAsync.o libHidDisplay.o
+
+ar -M <pack64.mri
+ranlib libhiddisplay.a
+
+del libhiddisplay_tmp.a
+del libHidDisplayAsync.o 
+del libHidDisplay.o
