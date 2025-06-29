61bddbb692ae2672d25a559ec13aca71faadb2e4
MichaelMcE <mapei_@hotmail.com>
2025-06-29 01:15:48 +0100

    Bring up to date

--- Examples/TGX/examples/Teensy4/3D/borg_cube/BUILD_64.bat
+++ Examples/TGX/examples/Teensy4/3D/borg_cube/BUILD_64.bat
@@ -1,4 +1,6 @@
 @echo off
 
-g++ -m64 -static -Ofast -I../../../../src borg_cube.cpp ../../../../../../libHidDisplay/libHidDisplayAsync.c ../../../../../../libHidDisplay/libHidDisplay.c ../../libs/libusb64.a -lwinmm -o borg_cube64.exe
+set HDLIBDIR=../../../../../../libHidDisplay
+set HDLIB=%HDLIBDIR%/libHidDisplay.c %HDLIBDIR%/libHidDisplayAsync.c
+g++ -m64 -static -Ofast -I%HDLIBDIR% %HDLIB% -I../../../../src borg_cube.cpp ../../libs/libusb64.a -lwinmm -o borg_cube64.exe
 strip borg_cube64.exe
--- Examples/TGX/examples/Teensy4/3D/borg_cube/borg_cube.cpp
+++ Examples/TGX/examples/Teensy4/3D/borg_cube/borg_cube.cpp
@@ -22,7 +22,7 @@
 #include <tgx.h> 
 #include <stdio.h>
 #include <string.h>
-#include "../../../../../../libHidDisplay/libHidDisplay.h"
+#include <libHidDisplay.h>
 
 #include <Renderer3D.h>
 #include <Renderer3D.cpp>
@@ -48,6 +48,7 @@ using namespace tgx;
 static teensyRawHidcxt_t ctx;
 
 
+static char DEVNAME[64];
 static int DWIDTH = 0;
 static int DHEIGHT = 0;
 static int SLX = DWIDTH;
@@ -133,13 +134,13 @@ void update ()
 {
 	static uint64_t tstart = GetTickCount();
 	
-	if (ctx.rgbClamp)
+	/*if (ctx.rgbClamp)
 		libHidDisplay_WriteImageEx(&ctx, fb, ctx.rgbClamp);
 	else
-		libHidDisplay_WriteImage(&ctx, fb);
+		libHidDisplay_WriteImage(&ctx, fb);*/
 	
-	//for (int y = 0; y < 480; y+=80)
-		//libHidDisplay_WriteArea(&ctx, &fb[y*854], 0, y, ctx.width-1, y+80-1);
+	for (int y = 0; y < ctx.height; y+=32)
+		libHidDisplay_WriteArea(&ctx, &fb[y*ctx.width], 0, y, ctx.width-1, y+32-1);
 	
 	
 	fcount++;
@@ -165,7 +166,7 @@ int openDisplayWait (const int timeMs)
 	for (int i = 0; i < loops; i++){
 		Sleep(delay);
 			
-		if (libHidDisplay_OpenDisplay(&ctx))
+		if (libHidDisplay_OpenDisplay(&ctx, 0))
 			return 1;
 	}
 	return 0;
@@ -173,7 +174,7 @@ int openDisplayWait (const int timeMs)
 
 int display_init ()
 {
-	if (!libHidDisplay_OpenDisplay(&ctx)){
+	if (!libHidDisplay_OpenDisplay(&ctx, 0)){
 		if (!openDisplayWait(500))
 			return 0;
 	}
@@ -185,7 +186,8 @@ int display_init ()
 	ctx.height = desc.u.cfg.height;
 	ctx.pitch = desc.u.cfg.pitch;
 	ctx.rgbClamp = desc.u.cfg.rgbMax;
-		
+	
+	strncpy(DEVNAME, (char*)(&desc.u.cfg.string[0]), 32);	
 	DWIDTH = ctx.width;
 	DHEIGHT = ctx.height;
 	
@@ -211,7 +213,7 @@ int main ()
 	if (!display_init())
 		return 0;
 	
-	printf("%i %i\n", SLX, SLY);
+	printf("Display: %s\nWxH: %ix%i\n", (char*)DEVNAME, SLX, SLY);
 
 		
 	setup();
