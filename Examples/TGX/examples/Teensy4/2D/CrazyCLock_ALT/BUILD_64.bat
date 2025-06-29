0c7c10b6e0b844bc6393c3819130ad6367ba0b08
MichaelMcE <mapei_@hotmail.com>
2025-06-29 01:14:17 +0100

    Bring up to date

--- Examples/TGX/examples/Teensy4/2D/CrazyCLock_ALT/BUILD_64.bat
+++ Examples/TGX/examples/Teensy4/2D/CrazyCLock_ALT/BUILD_64.bat
@@ -1,5 +1,7 @@
 @echo off
 
-g++ -m64 -static -I../../../../src -Ofast crazyclock.cpp green.cpp long_hand.cpp small_hand.cpp ../../../../../../libHidDisplay/libHidDisplay.c ../../../../../../libHidDisplay/libHidDisplayAsync.c ../../libs/libusb64.a -lwinmm -o CrazyClock64.exe
+set HDLIBDIR=../../../../../../libHidDisplay
+set HDLIB=%HDLIBDIR%/libHidDisplay.c %HDLIBDIR%/libHidDisplayAsync.c
+g++ -m64 -static -Ofast -I../../../../src crazyclock.cpp green.cpp long_hand.cpp small_hand.cpp -I%HDLIBDIR% %HDLIB% ../../libs/libusb64.a -lwinmm -o CrazyClock64.exe
 strip CrazyClock64.exe
 
--- Examples/TGX/examples/Teensy4/2D/CrazyCLock_ALT/CrazyClock.cpp
+++ Examples/TGX/examples/Teensy4/2D/CrazyCLock_ALT/CrazyClock.cpp
@@ -23,7 +23,7 @@
 #include <Color.cpp>
 #include <stdio.h>
 #include <string.h>
-#include "../../../../../../libHidDisplay/libHidDisplay.h"
+#include <libHidDisplay.h>
 
 
 
@@ -45,6 +45,7 @@ using namespace tgx;
 static teensyRawHidcxt_t ctx;
 
 
+static char DEVNAME[64];
 static int DWIDTH = 0;
 static int DHEIGHT = 0;
 static int SLX = DWIDTH;
@@ -122,7 +123,7 @@ int openDisplayWait (const int timeMs)
 	for (int i = 0; i < loops; i++){
 		Sleep(delay);
 			
-		if (libHidDisplay_OpenDisplay(&ctx))
+		if (libHidDisplay_OpenDisplay(&ctx, 0))
 			return 1;
 	}
 	return 0;
@@ -130,7 +131,7 @@ int openDisplayWait (const int timeMs)
 
 int display_init ()
 {
-	if (!libHidDisplay_OpenDisplay(&ctx)){
+	if (!libHidDisplay_OpenDisplay(&ctx, 0)){
 		if (!openDisplayWait(500))
 			return 0;
 	}
@@ -144,6 +145,7 @@ int display_init ()
 	
 	DWIDTH = ctx.width;
 	DHEIGHT = ctx.height;
+	strncpy(DEVNAME, (char*)(&desc.u.cfg.string[0]), 32);
 	
 	SLX = DWIDTH;
 	SLY = DHEIGHT;
@@ -167,10 +169,8 @@ int main ()
 	if (!display_init())
 		return 0;
 	
-	printf("%i %i\n", SLX, SLY);
+	printf("Display: %s\nWxH: %ix%i\n", (char*)DEVNAME, SLX, SLY);
 
-
-	
     int t;
 	uint64_t em = 0;
 	uint64_t time0 = GetTickCount();
