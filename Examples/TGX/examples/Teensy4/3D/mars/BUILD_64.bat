18c62986c89a3676d278a38582d214c0de4ccc06
MichaelMcE <mapei_@hotmail.com>
2025-06-29 01:16:50 +0100

    Bring up to date

--- Examples/TGX/examples/Teensy4/3D/mars/BUILD_64.bat
+++ Examples/TGX/examples/Teensy4/3D/mars/BUILD_64.bat
@@ -1,4 +1,6 @@
 @echo off
 
-g++ -m64 -static -Ofast -I../../../../src ../../../../../../libHidDisplay/libHidDisplayAsync.c ../../../../../../libHidDisplay/libHidDisplay.c mars.cpp ../../libs/libusb64.a -lwinmm -o mars64.exe
+set HDLIBDIR=../../../../../../libHidDisplay
+set HDLIB=%HDLIBDIR%/libHidDisplay.c %HDLIBDIR%/libHidDisplayAsync.c
+g++ -m64 -static -Ofast -I%HDLIBDIR% %HDLIB% -I../../../../src mars.cpp ../../libs/libusb64.a -lwinmm -o mars64.exe
 strip mars64.exe
--- Examples/TGX/examples/Teensy4/3D/mars/mars.cpp
+++ Examples/TGX/examples/Teensy4/3D/mars/mars.cpp
@@ -31,7 +31,7 @@
 #include <tgx.h> 
 #include <stdio.h>
 #include <string.h>
-#include "../../../../../../libHidDisplay/libHidDisplay.h"
+#include <libHidDisplay.h>
 
 
 using namespace tgx;
@@ -70,8 +70,8 @@ using namespace tgx;
 #define DMAMEM  
 #define elapsedMillis 	uint64_t
 
-
 static teensyRawHidcxt_t ctx;
+static char DEVNAME[64];
 static int DWIDTH = 0;
 static int DHEIGHT = 0;
 static int SLX = DWIDTH;
@@ -1206,7 +1206,7 @@ int openDisplayWait (const int timeMs)
 	for (int i = 0; i < loops; i++){
 		Sleep(delay);
 			
-		if (libHidDisplay_OpenDisplay(&ctx))
+		if (libHidDisplay_OpenDisplay(&ctx, 0))
 			return 1;
 	}
 	return 0;
@@ -1214,7 +1214,7 @@ int openDisplayWait (const int timeMs)
 
 int display_init ()
 {
-	if (!libHidDisplay_OpenDisplay(&ctx)){
+	if (!libHidDisplay_OpenDisplay(&ctx, 0)){
 		if (!openDisplayWait(500))
 			return 0;
 	}
@@ -1227,6 +1227,7 @@ int display_init ()
 	ctx.pitch = desc.u.cfg.pitch;
 	ctx.rgbClamp = desc.u.cfg.rgbMax;
 		
+	strncpy(DEVNAME, (char*)(&desc.u.cfg.string[0]), 32);
 	DWIDTH = ctx.width;
 	DHEIGHT = ctx.height;
 	
@@ -1252,7 +1253,7 @@ int main ()
 	if (!display_init())
 		return 0;
 	
-	printf("%i %i\n", SLX, SLY);
+	printf("Display: %s\nWxH: %ix%i\n", (char*)DEVNAME, SLX, SLY);
 	
 	setup();
 	timeBeginPeriod(1);	
